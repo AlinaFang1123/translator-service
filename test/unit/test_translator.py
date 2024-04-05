84b94a90d1ebfb21b267d5aac10f4ad2bbc9c5ae
AlinaFang1123 <128102805+AlinaFang1123@users.noreply.github.com>
2024-04-04 20:46:49 -0400

    modified translator.py and added translate_post.yml to work with Github secret and access Google API key

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -1,30 +1,30 @@
-from src.translator import translate_content
 import bigframes.dataframe
 import vertexai
 from mock import patch
 from unittest.mock import MagicMock
 import time
+from src.translator import get_translation
 
 @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
 def test_unexpected_language(mocker):
     mocker.return_value.text = "I don't understand your request"
 
-    assert translate_content("Aquí está su primer ejemplo.") == (True, 'Aquí está su primer ejemplo.')
+    assert get_translation("Aquí está su primer ejemplo.") == (True, 'Aquí está su primer ejemplo.')
 
 @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
 def test_malformed_response(mocker):
     mocker.return_value.text = '{"text": "This is a malformed response"'
 
-    assert translate_content("Aquí está su primer ejemplo.") == (True, 'Aquí está su primer ejemplo.')
+    assert get_translation("Aquí está su primer ejemplo.") == (True, 'Aquí está su primer ejemplo.')
 
 @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
 def test_timeout(mocker):
     mocker.return_value.text = MagicMock(side_effect=lambda: time.sleep(60) or "This is a slow response")
 
-    assert translate_content("Aquí está su primer ejemplo.") == (True, 'Aquí está su primer ejemplo.')
+    assert get_translation("Aquí está su primer ejemplo.") == (True, 'Aquí está su primer ejemplo.')
 
 @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
 def test_empty_response(mocker):
     mocker.return_value.text = {}
 
-    assert translate_content("Aquí está su primer ejemplo.") == (True, 'Aquí está su primer ejemplo.')
+    assert get_translation("Aquí está su primer ejemplo.") == (True, 'Aquí está su primer ejemplo.')
